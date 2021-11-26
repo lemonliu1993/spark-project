d51c7d5777abf4b9d9bc2682a7a4559aa04f987d
lemonliu1993 <2305247091@qq.com>
2021-11-26 23:58:49 +0800

    add resource

--- spark-core/src/main/java/com/lemon/spark/core/wc/Spark01_WordCount.scala
+++ spark-core/src/main/java/com/lemon/spark/core/wc/Spark01_WordCount.scala
@@ -1,5 +1,6 @@
 package com.lemon.spark.core.wc
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -13,6 +14,26 @@ object Spark01_WordCount {
     //JDBC:Connection
     val sparconf = new SparkConf().setMaster("local").setAppName("WordCount")
     val sc = new SparkContext(sparconf)
+
+    //TODO 执行业务操作
+
+    //1. 读取文件，获取一行一行的数据
+    //  hello world
+    val lines: RDD[String] = sc.textFile("datas")
+
+    //2.将一行数据进行拆分，形成一个一个的单词(分词)
+    //  "hello world" => hello,world,hello,world
+    val words: RDD[String] = lines.flatMap(_.split(" "))
+
+    //3.将数据根据单词进行分组，便于统计
+    // (hello,hello,hello),(world,world)
+    val wordGroip: RDD[(String, Iterable[String])] = words.groupBy(words => words)
+
+    //4.对分组后的数据进行转换
+    //  (hello,hello,hello),(world,world)
+    //  (hello,3),(world,2)
+
+    //5.将转换结果采集到控制台打印出来
 
     //TODO 关闭连接
     sc.stop()
